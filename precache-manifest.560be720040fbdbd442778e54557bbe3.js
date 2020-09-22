fd2324e4a69a1aee1a7673551c10b8c7b5bda4dc
ithaksinterlance <it.haksinterlance@gmail.com>
2020-09-22 17:00:43 +0530

    Updates

---
+++ precache-manifest.560be720040fbdbd442778e54557bbe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3bb611a7b708dd77897db3d52a00ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89bb3c2af95e615dcb16",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a324fd20ed7c9872cfee",
+    "url": "/static/css/main.cc188b54.chunk.css"
+  },
+  {
+    "revision": "89bb3c2af95e615dcb16",
+    "url": "/static/js/2.ab50ddf5.chunk.js"
+  },
+  {
+    "revision": "61182c055de86dcf23403d404d6a5a7e",
+    "url": "/static/js/2.ab50ddf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a324fd20ed7c9872cfee",
+    "url": "/static/js/main.b90fcd20.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
