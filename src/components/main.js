6000e8c84b352be998ac8109d6c1f72a7d40a030
ithaksinterlance <it.haksinterlance@gmail.com>
2020-09-22 18:22:21 +0530

    deploy the code

--- src/components/main.js
+++ src/components/main.js
@@ -9,12 +9,11 @@ import Post from "./post";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/products" component={Product} />
-    <Route path="/gallery" component={Gallery} />
-
-    <Route path="/contact" component={Contact} />
-    <Route path="/post" component={Post} />
+    <Route exact path="/portfolio" component={Home} />
+    <Route path="/portfolio/products" component={Product} />
+    <Route path="/portfolio/gallery" component={Gallery} />
+    <Route path="/portfolio/contact" component={Contact} />
+    <Route path="/portfolio/post" component={Post} />
   </Switch>
 );
 
